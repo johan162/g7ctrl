85b8dc3bca33ad36e6996fd5c77426c16f16b790
Johan Persson <johan162@gmail.com>
2018-10-11 23:31:53 +0200

    chg: ChangeLog is no longer auto-generated after switch-over to git

--- bldscripts/mkrelease.sh
+++ bldscripts/mkrelease.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 # Utility script to prepare a new release
-echo "Creating new ChangeLog ..."
-bldscripts/mkcl.sh
 echo "Updating code statistics ..."
 rm docs/manual/table-src-stats.xml
-echo "Running autoreconf ..."
-autoreconf
+#echo "Running autoreconf ..."
+#autoreconf
 if test "$?" = 0; then
 	echo "Running stdconfig ..."
 		bldscripts/stdconfig.sh > /dev/null
