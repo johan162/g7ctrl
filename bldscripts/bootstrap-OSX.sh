beccb54e64cceaf7a0c6009b036cf69b041f49c3
Johan Persson <johan162@gmail.com>
2022-06-18 00:29:39 +0200

    Added old OSX bootstrap script

---
+++ bldscripts/bootstrap-OSX.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+brew install autoconf
+brew install automake
+brew install libharu
+brew install readline
+brew install pcre
+brew install sqlite
+brew install libusb
+brew install xz
+
+# brew cask install java
+brew install libxslt
+brew install docbook-xsl
+brew install fop
+brew install gawk
+brew install cloc
+
+
+export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
+export LC_CTYPE=en_US.UTF-8
+
