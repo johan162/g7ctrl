ab7c28edf5284c1e9606ff7a89d0da5e66007de3
Johan Persson <johan162@gmail.com>
2022-06-22 00:06:27 +0200

    Update stst build

--- bldscripts/ljpbuild.sh
+++ bldscripts/ljpbuild.sh
@@ -5,5 +5,5 @@
 #
 # Created on Jan 8, 2015, 5:35:37 PM
 #
-#bldscripts/ststbuild.sh -s smtp.aditus.nu -u johanp@aditus.nu -p Psion3aPsion3a -f g7ctrl@aditus.nu -t johan162@gmail.com
-bldscripts/ststbuild.sh -s 192.168.0.50 -u ljp -p psion3s -f g7ctrl@aditus.nu -t johan162@gmail.com
+bldscripts/ststbuild.sh -s 192.168.0.50 -u $STST_SMTP_USER -p $STST_SMTP_PWD -f g7ctrl@aditus.nu -t johan162@gmail.com
+
