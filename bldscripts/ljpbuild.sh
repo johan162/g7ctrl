49ec0ec2a8ed7d7f2d7b41f746018d6704098d1f
Johan Persson <johan162@gmail.com>
2022-06-22 08:01:54 +0200

    Updated system test build script

--- bldscripts/ljpbuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# 
-# File:   ljp-stst.sh.bash
-# Author: ljp
-#
-# Created on Jan 8, 2015, 5:35:37 PM
-#
-bldscripts/ststbuild.sh -s 192.168.0.50 -u $STST_SMTP_USER -p $STST_SMTP_PWD -f g7ctrl@aditus.nu -t johan162@gmail.com
-
---
+++ bldscripts/ststenvbuild.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bldscripts/ststbuild.sh -s $STST_SMTP_SERVER -u $STST_SMTP_USER -p $STST_SMTP_PWD -f STST_SMTP_FROM -t STST_SMTP_TO
+
